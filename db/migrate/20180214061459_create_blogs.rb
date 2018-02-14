ab30a57f3d513a9fe099c7a6133322ca0b50d813
jiemeechong <jamiecjm@gmail.com>
2018-02-14 15:45:38 +0800

    Create navigation model

---
+++ db/migrate/20180214061459_create_blogs.rb
@@ -0,0 +1,18 @@
+class CreateBlogs < ActiveRecord::Migration[5.1]
+  def up
+    create_table :blogs do |t|
+    	t.string	:title
+    	t.string	:cover_photo
+    	t.text	:content
+    	t.boolean	:published?
+    	t.datetime	:published_at
+
+      t.timestamps
+    end
+  end
+
+  def down
+  	drop_table :blogs
+  end
+  
+end
---
+++ db/migrate/20180214061513_create_navigations.rb
@@ -0,0 +1,16 @@
+class CreateNavigations < ActiveRecord::Migration[5.1]
+  def up
+    create_table :navigations do |t|
+    	t.string	:name
+    	t.integer	:page_id
+    	t.string	:order
+
+      t.timestamps
+    end
+  end
+
+  def down
+  	drop_table :navigations
+  end
+  
+end
