3ff51d063528cfe232da139cccb937cae2cef16f
jiemeechong <jamiecjm@gmail.com>
2018-02-22 16:51:37 +0800

    Dyanmically set website title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :get_navigation
+  before_action :get_title
 
-  def get_navigation
-  	@navi_blogs = Blog.all
+  def get_title
+  	@path = request.fullpath.split('/').last
+  	if @path
+  		@title = "#{@path.gsub('-',' ').capitalize} | Serotonin Fix"
+  	else
+  		@title = 'Serotonin Fix'
+  	end
   end
+
 end
